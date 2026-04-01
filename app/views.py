53cbd0ce56c86241113699314709f6939014b1cd
Lucas Dias <13957748623@MUZAMBINHO.EDU.BR>
2026-03-31 22:14:45 -0300

    feat: criar vizualização da agenda de horários

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Equipe 
+from .models import Equipe, Agendamento
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+class EquipesView(View):
+    def get(self, request, *args, **kwargs):
+        # Busca todas as equipes no banco (Equipe.objects.all)
+        equipes = Equipe.objects.all() 
+        return render(request, 'equipes.html', {'equipes': equipes})
+
+class AgendamentosView(View):
+    def get(self, request, *args, **kwargs):
+        # Busca todos os agendamentos ordenados por data e hora
+        agendamentos = Agendamento.objects.all().order_by('data', 'horario_inicio')
+        return render(request, 'agendamentos.html', {'agendamentos': agendamentos})
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import IndexView
+from app.views import IndexView, EquipesView, AgendamentosView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
+    path('equipes/', EquipesView.as_view(), name='equipes'),
+    path('agendamentos/', AgendamentosView.as_view(), name='agendamentos'),
 ]
